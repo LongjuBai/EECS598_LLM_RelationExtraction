384900fdce67f9ae2ddf282a4a4158194cddfda9
Yiqun WANG <wyq@umich.edu>
2024-04-17 01:36:58 -0400

    ChatGPT ICL

--- run.py
+++ run.py
@@ -1,31 +1,110 @@
-from datasets.utils import load_conll04
+from utils import load_conll04, load_ade
+import json
+from tqdm import tqdm
+from openai import OpenAI
+import os
+import numpy as np
 
-def gpt_run_model(args, dataset_file, prompt_file, output_file):
+
+def get_response_from_gpt(args = {}, dataset='ade'):
     results = {}
-    with open(prompt_file, 'r') as f:
-        prompt_ = f.read()
+    prompt_file_path = os.path.join('prompts', dataset, 'prompt.txt')
     
-    with open(dataset_file, 'r') as f:
-        dataset = json.load(f)
+    with open(prompt_file_path, 'r') as f:
+        prompt = f.read()
+    
+    test_data = load_ade()['test']
+
+    client = OpenAI(api_key='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')
+    
+    # metrics initialization
+    micro_F1_list = []
+    false_pred_num, missing_true_num, total_pred_num, total_true_num = 0, 0, 0, 0
+
+    # fail case saving initialization
+    correct_cases = {} # all correct
+    fail_cases = {}
+    missing_cases = {}
+    false_cases = {}
+    for i, (id, data) in tqdm(enumerate(test_data.items())):
+        prompt_message = prompt.replace('$TEXT$', data['text']),
+        response = client.completions.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=prompt_message,
+            max_tokens=100,
+            temperature=0.5
+        )
+        predicted_string_processed = response.choices[0].text.replace("'", '"').lower()
+        true_set = set([tuple([item.replace("'", '"').lower() for item in relation]) for relation in data['relations']])
+        try:
+            predicted_list = [tuple(relation) for relation in json.loads(predicted_string_processed)] # result is a list of list
+            predicted_set = set(predicted_list)
+            precision = len(predicted_set.intersection(true_set))/len(predicted_set) # 
+            recall = len(true_set.intersection(predicted_set))/len(true_set)
+            micro_F1_list.append(2/(1/precision + 1/recall) if min(precision, recall) > 0 else 0)
+            false_pred_num += len(predicted_set.intersection(true_set))
+            total_pred_num += len(predicted_set)
+            missing_true_num += len(true_set.intersection(predicted_set))
+        except Exception as e:
+            micro_F1_list.append(0)
+            print("error in try: ", e)
+            print("LLM message:", response.choices[0].text)
+            # false_pred_num += 0
+            # total_pred_num += 0
+            # missing_true_num += 0
+        total_true_num += len(true_set)
         
-    if 'gpt-4' in args.model_name or "gpt-3.5-turbo-1106" == args.model_name:
-        gpt_func = gpt_chat
-    else:
-        gpt_func = gpt_instruct
-    
-    source_texts = list(dataset.keys())
-    for i in tqdm(range(len(source_texts))):
-        # try:
-        source_text = source_texts[i]
-        prompt = prompt_.replace('$TEXT$', source_text)
-        generation = gpt_func(args.model_name, prompt, args.seed)
-        # relation_str = post_processing(args.model_name, generation)
-        results[source_text] = generation
-        if i % 20  == 0:
-            with open(output_file, 'w') as f:
-                json.dump(results, f, indent=6)
+        # record fail/correct cases
+        if true_set == predicted_set:
+            correct_cases[id] = predicted_set
+        else:
+            fail_cases[id] = predicted_set
+            missing_cases[id] = true_set.difference(predicted_set)
+            false_cases[id] = predicted_set.difference(true_set)
+        
+        # early termination
+        if i >= 5:
+            break
+
+
+    precision = false_pred_num/total_pred_num
+    recall = missing_true_num/total_true_num
+    macro_F1 = 2/(1/precision + 1/recall)
+    micro_F1 = np.mean(micro_F1_list)
+
+    output = {
+        'precision': precision,
+        'recall': recall,
+        'macro_F1': macro_F1,
+        'micro_F1': micro_F1,
+        'accuracy': len(correct_cases)/(len(correct_cases) + len(fail_cases)),
+        'correct_cases': correct_cases, # all correct
+        'fail_cases': fail_cases,
+        'missing_cases': missing_cases,
+        'false_cases': false_cases
+    }
+
+    with open('output.json', 'w') as json_file:
+        json.dump(output, json_file)
+        # save if wrong
+
+        # print("text: ", data['text'])
+        # print("true relations:", data['relations'])
+        # print("result relations", results[id])
+    # source_texts = list(dataset.keys())
+    # for i in tqdm(range(len(source_texts))):
+    #     # try:
+    #     source_text = source_texts[i]
+    #     prompt = prompt_.replace('$TEXT$', source_text)
+    #     generation = gpt_func(args.model_name, prompt, args.seed)
+    #     # relation_str = post_processing(args.model_name, generation)
+    #     results[source_text] = generation
+    #     if i % 20  == 0:
+    #         with open(output_file, 'w') as f:
+    #             json.dump(results, f, indent=6)
         # except:
         #     print(f'error occured at {i}')
         #     continue
-        
     return results
+if __name__ == "__main__":
+    get_response_from_gpt()
