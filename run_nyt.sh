01ce3319053aaad4322d1de4ef6652b50b9ae76d
blj <849392857@qq.com>
2024-06-09 15:37:30 -0400

    nyt test

--- run_nyt.sh
+++ run_nyt.sh
@@ -1,9 +1,5 @@
 for i in {42..42}
 do
     echo "Running $i"
-<<<<<<< Updated upstream
     python tot2_multi.py --dataset nyt --seed $i --suffix 'umgpt-testing' --model 'umgpt' --test_k 10
-=======
-    python main.py --dataset nyt --seed $i --test_k 100
->>>>>>> Stashed changes
 done
