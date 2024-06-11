57baa9bdf4e6a44ef8af4322e4d894bf4a7573d0
Yicheng Tao <yctao@umich.edu>
2024-06-11 16:48:06 -0400

    merge

--- run_conll04.sh
+++ run_conll04.sh
@@ -1,5 +1,6 @@
 for i in {42..42}
 do
     echo "Running $i"
-    python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2_async --part test --model umgpt --api_key api_key_umgpt --is_async --test_k -1
+    # python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2_async --part test --model umgpt --api_key api_key_umgpt --is_async --test_k -1
+    python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2 --part test --model umgpt --api_key api_key_umgpt --test_k -1
 done
