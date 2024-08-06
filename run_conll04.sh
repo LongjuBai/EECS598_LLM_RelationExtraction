6481c666d152a381c4cb225fceeba0267e52fd24
Yiqun WANG <wyq@umich.edu>
2024-08-06 12:00:14 -0500

    5 entities 4 prior para 41 sentences

--- run_conll04.sh
+++ run_conll04.sh
@@ -2,5 +2,5 @@ for i in {42..42}
 do
     echo "Running $i"
     # python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2_async --part test --model umgpt --is_async --test_k -1
-    python tot2_multi.py --dataset conll04 --seed $i --suffix conll04_test_noPrior_icl_5entities4priorpara16sentences --part test --model umgpt --do_paraphrase --test_k -1 --number_of_shots_for_entity_extraction 5 --number_of_shots_for_relation 16
+    python tot2_multi.py --dataset conll04 --seed $i --suffix conll04_test_noPrior_icl_5entities4priorpara41sentences --part test --model umgpt --do_paraphrase --test_k -1 --number_of_shots_for_entity_extraction 5 --number_of_shots_for_relation 41
 done
