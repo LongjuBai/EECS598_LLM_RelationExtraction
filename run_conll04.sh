12a8c49d3876863e33d4c5baba239938ebb1d9b7
wangyiqunumich <57782606+wangyiqunumich@users.noreply.github.com>
2026-02-02 16:49:20 -0500

    Update script to run with new parameters

--- run_conll04.sh
+++ run_conll04.sh
@@ -1,6 +1,5 @@
 for i in {42..42}
 do
     echo "Running $i"
-    # python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2_async --part test --model umgpt --is_async --test_k -1
-    python tot2_multi.py --dataset conll04 --seed $i --suffix conll04_test_noPrior_icl_3entities4priorpara3sentences --part test --model umgpt --do_paraphrase --test_k -1 --use_icl --number_of_shots_for_entity_extraction 3 --number_of_shots_for_relation 3
+    python tot2_multi.py --dataset conll04 --seed $i --suffix umgpttesting2 --part test --model umgpt --test_k -1
 done
